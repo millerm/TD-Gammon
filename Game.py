85789127d59a82bb2175a9ad33e127c8aa247ada
millerm <marshalldavidmiller@gmail.com>
2017-02-23 11:53:37 -0700

    Added get moves

--- Game.py
+++ Game.py
@@ -38,68 +38,107 @@ class Game:
 
 
     def find_moves(self, roll, player):
+        moves = []
+
         for i in range(len(self.board)):
             self.print_point(i)
+
+        # Probably can get rid of this and just reference the indices of the roll
         r1, r2 = roll[0], roll[1]
 
-        # Determine how many rolls
-        # Did we roll doubles?
+        j = 2
+
+        # Did we roll doubles? Alter j to 4 if this is to be handled
         if r1 == r2:
-            print("Player {} rolled double {}'s!".format(player, r1))
-            j = 2
-        else:
-            j = 2
+            print("Player {} rolled double {}'s! But we won't worry about that".format(player, r1))
 
-        print("R1 = {}".format(r1))
-        # Are there pieces on the bar for the given player?
-        if len(self.on_bar[player]) >= 1:
-            # Does the point where we would put the piece have no pieces or is it controlled by the player?
-            if self.board[r1-1] <=1 or self.board[len(self.board[r1-1])- 1 ] == player:
-                # Does the point where we would put the piece have 1 stone that is the opposite player's color?
-                if self.board[r1-1] == 1 and self.board[len(self.board[r1-1])- 1] != player:
-                    piece = self.on_bar[player].pop()
-                    if self.board[r1-1] == get_opponent(player):
-                        hit = self.board[r1-1].pop()
-                        self.on_bar[get_opponent(player)].append(hit)
-                        self.board[r1-1].append(piece);
+        for i in range(j):
+            k = i - 1
+            r = roll[k]
+            print("R = {}".format(r))
+            # Are there pieces on the bar for the given player?
+            if len(self.on_bar[player]) >= 1:
+                print("Player {} has pieces on the bar".format(self.on_bar[player]))
+                # Does the point where we would put the piece have no pieces or is it controlled by the player?
+                if self.board[r] <=1 or self.board[len(self.board[r])- 1 ] == player:
+                    # Does the point where we would put the piece have 1 stone that is the opposite player's color?
+                    if self.board[r] == 1 and self.board[len(self.board[r])- 1] != player:
+                        # piece = self.on_bar[player].pop()
+                        if self.board[r] == get_opponent(player):
+                            # hit = self.board[r1-1].pop()
+                            # self.on_bar[get_opponent(player)].append(hit)
+                            # self.board[r1-1].append(piece);
+                            moves.append(('bar', r))
 
-            piece = self.on_bar[p].pop()
-            self.board[r1-1].append(piece)
+                #piece = self.on_bar[p].pop()
+                #self.board[r].append(piece)
 
-        print("Player {} has no pieces on the bar".format(player))
-        print("player {} opponent is {}".format(player, self.get_opponent(player)))
+            print("Player {} has no pieces on the bar".format(player))
 
-        # Can the player hit the other player?
-        for i in range(len(self.board)):
+            # Can we bear off?
+            if player == 'white':
+                for i in range(len(self.board) - 1):
+                    if len(self.board[i]) > 0:
+                        if self.board[i][0] == player:
+                            if (i + r) > len(self.board):
+                                print("Player {} can bear off!".format(player))
+                                moves.append((i, 'off'))
+
+            # Can the player hit the other player?
+            for i in range(len(self.board) - 1):
+                if len(self.board[i]) > 0:
+                    if self.board[i][0] == player:
+                        # Check to see if the point a roll away has 1 opponent piece
+                        if (i + r) <= len(self.board) -1:
+                            if len(self.board[i + r]) == 1 and self.board[i + r][0] == self.get_opponent(player):
+
+                                print("Player {} can hit on point {}".format(player, [i + r]))
+                                #hit_piece = self.board[i + r].pop()
+                                #self.on_bar[self.get_opponent(player)].append(hit_piece)
+                                #moved_piece = self.board[i].pop()
+                                #self.board[i + r].append(moved_piece)
+                                moves.append((i, i + r))
+                                # print("Player {} has {} pieces on the bar".format(self.get_opponent(player), len(self.on_bar[self.get_opponent(player)])))
+                                # for i in range(len(self.board)):
+                                #     self.print_point(i)
+
+            # Can the player make a valid move with the roll?
+            for i in range(len(self.board) - 1):
+                if len(self.board[i]) > 0:
+                    if self.board[i][0] == player:
+                        if i + r <= len(self.board) - 1:
+                            if len(self.board[i + r]) == 0 or self.board[i + r][0] == player:
+                                move = (i, i + r)
+                                # moves.append[(i, i + r)]
+                                moves.append(move)
+
+        # Can the player make a valid move with the combination of rolls?
+        combo = r1 + r2
+        for i in range(len(self.board) - 1):
             if len(self.board[i]) > 0:
                 if self.board[i][0] == player:
-                    # Check to see if the point a roll away has 1 opponent piece
-                    if i + r1 <= len(self.board):
-                        if len(self.board[i + r1]) == r1 and self.board[i + r1][0] == self.get_opponent(player):
+                    # Check to see if a valid move w/ roll 1 can be made to to bear off with combo
+                    if (i + r1 <= len(self.board)) and (i + combo> len(self.board)):
+                        moves.append((i, 'off'))
+                    # Check validity of rest of moves
+                    if i + combo <= len(self.board):
+                        if len(self.board[i + combo]) <= 1:
+                            # When taking action, check for hit!
+                            moves.append((i, i + combo))
 
-                            print("Player {} can hit on point {}".format(player, [i + r1]))
-                            hit_piece = self.board[i + r1].pop()
-                            self.on_bar[self.get_opponent(player)].append(hit_piece)
-                            moved_piece = self.board[i].pop()
-                            self.board[i + r1].append(moved_piece)
+        print("Player {} possible moves: {}".format(player, moves))
 
-                            print("Player {} has {} pieces on the bar".format(self.get_opponent(player), len(self.on_bar[self.get_opponent(player)])))
-                            for i in range(len(self.board)):
-                                self.print_point(i)
-
-        # Can the player move? 
-
-
-
-    def is_valid_move(self, start, end, piece_color):
-        if len(self.board[start]) > 0 and self.board[start] == piece_color:
-            if end < 0 or end >= len(self.board):
-                return False
-            if len(self.board[end]) <= 1:
-                return True
-            if len(self.board[end]) > 1 and self.board[end] == piece_color:
-                return True
-        return False
+    # def take_action(self, player, action):
+        # for i in range(len(self.board)):
+        #     if len(self.board[i]) > 0:
+        #         if self.board[i][0] == player:
+        #             if i + r1 <= len(self.board):
+        #                 if len(self.board[i + r1]) == 0 or self.board[i + r1][0] == player:
+        #                     print("moving a piece from point {} to point {}".format(i, i + r1))
+        #                     piece = self.board[i].pop()
+        #                     self.board[i + r1].append(piece)
+        #                     for i in range(len(self.board)):
+        #                         self.print_point(i)
 
     # Check to see if the game is over
     def game_over(self):
